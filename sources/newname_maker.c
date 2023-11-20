9a594885aba158f9920680ebba8cdd62e145a020
sua918 <ssua918@naver.com>
2023-11-21 02:07:06 +0900

    C프로젝트 기능 1.1

--- sources/newname_maker.c
+++ sources/newname_maker.c
@@ -1,5 +1,7 @@
 ﻿#include <stdio.h>
 
+int nameMaker(userName);
+
 int main(void) {
   printf(
       "=========================================================\n\n"
@@ -7,14 +9,27 @@ int main(void) {
       "사용하고자 하는 자신의 고유 닉네임을 입력해주세요.\n\n"
       "=========================================================\n\n");
   char nickname[20];
+  char nameStyle[4][50] = {"본래 닉네임과 근접한 닉네임", "웃긴 닉네임",
+                           "의미없는 닉네임", "특수문자로 꾸민 닉네임"};
+  printf("자신의 닉네임: ");
   scanf_s("%s", nickname, sizeof(nickname));
-  printf("\n입력된 닉네임 : %s\n\n", nickname);
+  printf("\n입력된 닉네임: %s\n\n", nickname);
   printf(
       "당신은 어떤 스타일의 닉네임을 선호하시나요?\n"
       "다음의 종류 중 원하는 것을 선택하세요.\n\n"
-      "1. 본래 닉네임과 근접한 닉네임  2. 웃긴 닉네임  3. 의미없는 닉네임  4. "
-      "기호로 꾸민 닉네임\n\n");
+      "1. %s  2. %s  3. %s  4. "
+      "%s\n\n",
+      nameStyle[0], nameStyle[1], nameStyle[2], nameStyle[3]);
   int choice;
+  printf("종류 선택 (1-4): ");
   scanf_s("%d", &choice);
-  printf("\n선택된 종류 : %d", choice);
+  printf("\n선택된 종류: %d. %s", choice, nameStyle[choice-1]);
+
+
+
+
+}
+
+int nameMaker(userName) { 
+    // TO-DO: 구조체 배우고 나서 만들 것
 }
