c8109a794489d6e2771879bdb15e1d9a1e26e1f1
sua918 <ssua918@naver.com>
2023-12-10 00:30:19 +0900

    사용자 새 닉네임 생성 기능 1.5

--- sources/main_newname_maker.c
+++ sources/main_newname_maker.c
@@ -3,24 +3,25 @@
 #include <string.h>
 #include <time.h>
 
-char* nameMaker1(char *nickname, char *name_deco);
+char *nameMaker1(char *nickname, char *name_deco);
 char *nameMaker2(char *nickname, char (*name_deco)[20], int rows);
-void nameMaker3(char *nickname, char *name_deco, int position);
-void nameMaker4(char *nickname, char *name_deco, int position);
-
-int nameLen;
+char *nameMaker3(char *nickname, char *name_deco);
 
 char nickname[21];
-char name_style[4][50] = {"본래 닉네임과 비슷한 닉네임", "웃긴 닉네임",
-                          "의미없는 닉네임", "특수문자로 꾸민 닉네임"};
+char name_style[3][50] = {"본래 닉네임과 비슷한 닉네임", "웃긴 닉네임",
+                          "의미없는 닉네임"};
 char name_deco1[4] = {'.', ',', '_', '`'};
-char name_deco2[18][20] = {"Hilarious", "Comical",       "Amusing",   "Witty",
-                           "Laughable", "Silly",         "Goofy",     "Zany",
-                           "Slapstick", "Sidesplitting", "Dumb",      "Foolish",
-                           "Dopey",     "Dimwitted",     "Brainless", "Dense",
-                           "Pigheaded", "Blockheaded"};
-char name_deco3[100] = {""};
-char name_deco4[100] = {""};
+char name_deco2[18][20] = {"Hilarious_", "Comical_",       "Amusing_",   "Witty_",
+                           "Laughable_", "Silly_",         "Goofy_",     "Zany_",
+                           "Slapstick_", "Sidesplitting_", "Dumb_",      "Foolish_",
+                           "Dopey_",     "Dimwitted_",     "Brainless_", "Dense_",
+                           "Pigheaded_", "Blockheaded_"};
+char name_deco3[61] = {'1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B',
+                        'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+                        'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+                        'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                        'u', 'v', 'w', 'x', 'y', 'z'};
 
 int main(void) {
   printf(
@@ -35,17 +36,16 @@ int main(void) {
   printf(
       "당신은 어떤 스타일의 닉네임을 선호하시나요?\n"
       "다음의 종류 중 원하는 것을 선택하세요.\n\n"
-      "1. %s  2. %s  3. %s  4. "
-      "%s\n\n",
-      name_style[0], name_style[1], name_style[2], name_style[3]);
+      "1. %s  2. %s  3. %s\n\n",
+      name_style[0], name_style[1], name_style[2]);
 
   int choice;
 
-  printf("종류 선택 (1-4): ");
+  printf("종류 선택 (1-3): ");
   scanf_s("%d", &choice);
   printf("\n선택된 종류: %d. %s\n\n", choice, name_style[choice - 1]);
 
-  srand(time(NULL));                 // 난수 생성이 가능하게 함
+  srand(time(NULL));  // 난수 생성이 가능하게 함
 
   if (choice == 1) {
     char *new_nickname = nameMaker1(nickname, name_deco1);
@@ -57,16 +57,15 @@ int main(void) {
       free(new_nickname);
     }
   } else if (choice == 3) {
-    //nameMaker3(nickname, name_deco3[rand_deco], position);
-  } else if (choice == 4) {
-    //nameMaker4(nickname, name_deco4[rand_deco], position);
+    char *new_nickname = nameMaker3(nickname, name_deco3);
+    printf("%s\n", new_nickname);
   }
 
   return 0;
 }
 
 // 한글로 하면 작동이 잘 안됨. 영어 닉네임으로 바꾸든가 고민을 더 해봐야겠다.
-char* nameMaker1(char *nickname, char *name_deco) {
+char *nameMaker1(char *nickname, char *name_deco) {
   int name_len = strlen(nickname);
   int deco_len = strlen(name_deco);
   int position = rand() % name_len;
@@ -92,17 +91,21 @@ char *nameMaker2(char *nickname, char (*name_deco)[20], int rows) {
 
   strcpy_s(result, size, name_deco[rand_deco]);
   strcat_s(result, size, nickname);
+
   return result;
 }
 
-void nameMaker3(char *nickname, char *name_deco, int position) {
+char *nameMaker3(char *nickname, char *name_deco) {
   int name_len = strlen(nickname);
+  int deco_len = strlen(name_deco);
+  int position = rand() % name_len;
+  int rand_deco = rand() % deco_len;
 
   // 문자열의 끝부터 시작하여 position까지 문자열 이동
   for (int i = name_len; i >= position; i--) {
     nickname[i + 1] = nickname[i];
   }
-  nickname[position] = name_deco;
-}
+  nickname[position] = name_deco[rand_deco];
 
-void nameMaker4(char *nickname, char *name_deco, int position) {}
+  return nickname;
+}
