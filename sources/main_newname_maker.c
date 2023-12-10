b88fbd455215a311538fac5152a3c41e2d46b217
sua918 <ssua918@naver.com>
2023-12-10 15:06:54 +0900

    코딩스타일 수정 및 예외처리 추가

--- sources/main_newname_maker.c
+++ sources/main_newname_maker.c
@@ -11,9 +11,9 @@
 #define NUM_NAME_STYLE 3
 #define NUM_NAME_DECO2 18
 
-char *nameMaker1(char *nickname, char *name_deco);
-char *nameMaker2(char *nickname, char (*name_deco)[20], int rows);
-char *nameMaker3(char *nickname, char *name_deco);
+char *NameMakerChar(char *nickname, char *name_deco);
+char *NameMakerStr(char *nickname, char *name_deco, int rows);
+void FreeMemory(char **new_nicknames);
 
 char *new_nicknames[NUM_NICKNAMES];
 
@@ -41,8 +41,9 @@ int main(void) {
         "=========================================================\n\n"
         "영문 닉네임 자동 추천 프로그램, '뉴네임메이커'가 실행되었습니다.\n"
         "사용하고자 하는 자신의 고유 영문 닉네임을 입력해주세요. (최대 "
-        "20자)\n\n"
-        "=========================================================\n\n");
+        "%d자)\n\n"
+        "=========================================================\n\n",
+        MAX_NICKNAME_LENGTH);
 
     printf("자신의 닉네임: ");
     scanf_s("%20s", nickname, (int)sizeof(nickname));
@@ -55,19 +56,25 @@ int main(void) {
         name_style[0], name_style[1], name_style[2]);
 
     int choice;
-    printf("종류 선택 (1-3): ");
-    scanf_s("%d", &choice);
-    int ch = getchar();
+    while (1) {
+      printf("종류 선택 (1-3): ");
+      if (scanf_s("%d", &choice) != 1 || choice < 1 || choice > 3) {
+        printf("\n잘못된 선택입니다. 1부터 3까지의 숫자를 선택해주세요.\n\n");
+        while (getchar() != '\n')
+          ;
+      } else
+        break;
+    }
 
     printf("\n선택된 종류: %d. %s\n\n", choice, name_style[choice - 1]);
 
     for (int i = 0; i < NUM_NICKNAMES; i++) {
       if (choice == 1) {
-        new_nicknames[i] = nameMaker1(nickname, name_deco1);
+        new_nicknames[i] = NameMakerChar(nickname, name_deco1);
       } else if (choice == 2) {
-        new_nicknames[i] = nameMaker2(nickname, name_deco2, 18);
+        new_nicknames[i] = NameMakerStr(nickname, name_deco2, 18);
       } else if (choice == 3) {
-        new_nicknames[i] = nameMaker3(nickname, name_deco3);
+        new_nicknames[i] = NameMakerChar(nickname, name_deco3);
       }
     }
 
@@ -76,29 +83,36 @@ int main(void) {
         printf("새 닉네임 %d: %s\n", i + 1, new_nicknames[i]);
       }
     }
-    for (int i = 0; i < NUM_NICKNAMES; i++) {  // 동적 할당 된 메모리 해제
-      free(new_nicknames[i]);
-    }
+    FreeMemory(new_nicknames);
 
     int end;
-    printf(
-        "\n\n마음에 들지 않는 경우 1을 입력하여 닉네임을 처음부터 다시 만들 수 "
-        "있습니다.\n"
-        "새 닉네임이 마음에 든다면 0을 입력하여 프로그램을 "
-        "종료하세요...\n\n");
-    scanf_s("%d", &end);
+    while (1) {
+      printf(
+          "\n새 닉네임이 마음에 들지 않는 경우 0을 입력하여 닉네임을 처음부터 "
+          "다시 만들 수 "
+          "있습니다.\n"
+          "새 닉네임이 마음에 든다면 1을 입력하여 프로그램을 "
+          "종료하세요.\n\n");
+      printf("0 또는 1을 선택하세요: ");
+      if (scanf_s("%d", &end) != 1 || (end != 0 && end != 1)) {
+        printf("\n잘못된 선택입니다. 0 또는 1을 선택해주세요.\n");
+        while (getchar() != '\n')
+          ;
+      } else
+        break;
+    }
 
-    if (end == 1) {
-      printf("\n\n");
-      ch = getchar();
+    if (end == 0) {
+      printf("\n");
+      getchar();
       continue;
-    } else if (end == 0)
+    } else if (end == 1)
       break;
   }
   return 0;
 }
 
-char *nameMaker1(char *nickname, char *name_deco) {
+char *NameMakerChar(char *nickname, char *name_deco) {
   int name_len = strlen(nickname);
   int deco_len = strlen(name_deco);
   int position = rand() % name_len;
@@ -122,7 +136,8 @@ char *nameMaker1(char *nickname, char *name_deco) {
   return result;
 }
 
-char *nameMaker2(char *nickname, char (*name_deco)[20], int rows) {
+char *NameMakerStr(char *nickname, char (*name_deco)[MAX_NAME_DECO_LENGTH],
+                   int rows) {
   int rand_deco = rand() % rows;
   int size = strlen(nickname) + strlen(name_deco[rand_deco]) + 1;
   char *result = (char *)malloc(size);
@@ -137,26 +152,9 @@ char *nameMaker2(char *nickname, char (*name_deco)[20], int rows) {
   return result;
 }
 
-char *nameMaker3(char *nickname, char *name_deco) {
-  int name_len = strlen(nickname);
-  int deco_len = strlen(name_deco);
-  int position = rand() % name_len;
-  int rand_deco = rand() % deco_len;
-  int size = strlen(nickname) + 2;
-  char *result = (char *)malloc(size);
-  char new_nickname[30];
-  strcpy_s(new_nickname, 30, nickname);
-
-  if (result == NULL) {
-    return NULL;
+void FreeMemory(char **new_nicknames) {
+  for (int i = 0; i < NUM_NICKNAMES; i++) {
+    free(new_nicknames[i]);
+    new_nicknames[i] = NULL;
   }
-
-  // 문자열의 끝부터 시작하여 position까지 문자열 이동
-  for (int i = name_len; i >= position; i--) {
-    new_nickname[i + 1] = new_nickname[i];
-  }
-  new_nickname[position] = name_deco[rand_deco];
-  strcpy_s(result, size, new_nickname);
-
-  return result;
 }
